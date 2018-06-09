1c861c7df88d2cc30390fb27eb81c397304df0b4
Adriano Xavier <aj-dx@hotmail.com>
2018-06-09 11:55:06 -0300

    Cria loginController (1).

---
+++ Controller/loginController.php
@@ -0,0 +1,37 @@
+<?php
+	require_once('..\DAO\conexao.inc');
+	require_once('..\DAO\ClienteDAO.php');
+	require_once('..\Model\Cliente.php');
+
+	$email = $_REQUEST['email'];
+	$senha = $_REQUEST['senha'];
+
+	$user = new Cliente();
+	$clienteDao = new ClienteDAO();
+	$user = $clienteDao->efetuarLogin($email, $senha);
+	var_dump($user);
+	
+	if($user==false)
+	{	
+		$erro = true;
+		header("Location:../login.php?erro=".$erro);
+	}
+	else
+	{
+	    if ($user->Tipo_user==0)
+		{
+			session_start();
+			$_SESSION['Logado'] = true;
+			$_SESSION['usuario'] = $user;
+			header("Location:restrito/areaRestrita.php");
+		}
+		else if ($user->Tipo_user==1) 
+		{
+			session_start();
+			$_SESSION['Logado'] = true;
+			$_SESSION['usuario'] = $user;
+			header("Location:index.php");
+		}
+	}
+
+?>
